249fdd0abbcf82aa537b67f4e8401b52b617e462
yoihenken <bagusbayu0310tkj1@gmail.com>
2020-11-27 14:07:14 +0700

    fill data data-oleh

--- app/data/data-oleh.js
+++ app/data/data-oleh.js
@@ -1,5 +1,97 @@
 var listOleh = [
     {
-        
+        title : "Mug Tanah Liat",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Mug-Tanah-Liat.jpg",
+        content : "Memberikan buah tangan untuk saudara dan keluarga di rumah, tak melulu soal makanan. Pasalnya, anda bisa membeli mug yang terbuat dari tanah liat ini ketika sedang mengunjungi kota Tegal. Souvenir satu ini tentu saja akan sangat cocok untuk anda gunakan sebagai oleh-oleh untuk orang tercinta. Anda pun dapat dengan mudah untuk menemukannya karena mug ini tersebar di beberapa toko souvenir di daerah Tegal. Untuk itu, anda pun bisa membelinya sebagai salah satu oleh-oleh yang unik dan khas yang hanya bisa untuk anda temui di daerah Tegal. Untuk harganya pun juga tidak terlalu mahal dan ramah di kantong sehingga anda pun bisa membelinya dengan jumlah yang cukup banyak dan bisa untuk anda bagikan dengan kerabat di rumah. Anda pun bisa membelinya di Raja Souvenir Tegal. "
+    },
+    {
+        title : "Poci Tegal",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Mug-Tanah-Liat.jpg",
+        content : "Selain mug tanah liat, maka anda pun juga bisa untuk membawa pulang poci khas Tegal yang memnag telah terkenal. Untuk anda yang tertarik, maka harga untuk satu set poci tega ini dapat dijual dengan kisaran harga sekitar 80 ribu rupiah saha. Anda pun dapat membeli satu set poci ini di daerah pengrajin poci yang terda[at di daerah Talang. Atau and apun juga bisa membelinya di dalam Mall ataupun suprmarket seperti Pacific Mall, Rital Mall dan juga Mutiara Cahaya."
+    },
+    {
+        title : "Kaos Galgil",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Kaos-Galgil.jpg",
+        content : "Selain alat untuk minum, maka anda pun juga bisa untuk membeli buah tangan khas kota Tegal yakni Kaos Galgil yang sangat populer di kota satu ini. Disini, anda pun dapat dengan mudah untuk memesan motifnya. Selain itu, anda pun juga bisa dengan mudah untuk memilih ragam motif maupun desain yang telah tersedia sehingga nantinya anda pun akan lebih mudah untuk memilihnya. Untuk membelinya, anda bisa mengunjungi pusat toko yang berada di jalan Mangkukusuman."
+    },
+    {
+        title : "Tahu Aci",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/36cd0c9a178574404776c43cbb2c1e25.jpg",
+        content : "Tidak sulit menemukan penjual tahu aci yang gurih, enak dan kenyal ini di kota Tegal. Hampir di sepanjang jalan kamu akan melihat banyak pedagang yang menjual tahu aci, baik dalam gerobak maupun toko oleh-oleh. Harganya berkisar Rp2.500 untuk satu biji tahu aci."
+    },
+    {
+        title : "Tahu pletok",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/3b083c086f0d2c852923bd6fb1f20234.jpg",
+        content : "Jika tahu aci dibuat dengan cara membagi tahu kotak menjadi dua bagian dengan bentuk segitiga, tahu pletok terbuat dari potongan tahu aci yang dibelah-belah kembali, tetapi potongannya tidak sampai terlepas. Rasanya tidak kalah gurih dari tahu aci. Biasanya pedagang yang menjual tahu aci juga akan menjual tahu pletok. Harga tahu pletok tidak berbeda dengan tahu aci, yaitu Rp2.500 per biji."
+    },
+    {
+        title : "Latopia",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Latopia.jpg",
+        content : "Makanan satu ini merupakan salah satu oleh-oleh yang waijib untuk anda bawa pulang ketika sedang mengunjungi kota Tegal. Di kota Tegal sendiri juga memiliki bakpia yang terkenal dengan sebutan Latopia. Untuk permbedanya sedniri adalah terletak apda ukurannya yang mmepunyai ukuran lebih besar dan memiliki kulit pia yang cenderung lebih gurih dan renyah. Untuk teksturnya sendiri, Latopia ini memiliki bentuk yang lebih padat. Makanan satu ini pun dapat dengan mudah untuk anda temukan di kota Tegal. Anda bisa mendapatkannya di toko oleh-oleh atau juga ke pasar tradisional yakni pasar pagi tegal yang brada di blok A. Untuk harganya sendiri, biasnaya Latopia ini dibanderol dengan harga sekitar 15 ribu rupiah dengan isi satu plastik berisi 10 buah Latopia."
+    },
+    {
+        title : "Kerupuk Antor",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Kerupuk-Antor.jpg",
+        content : "Buah tangan satu ini juga menjadi slah satu oleh-oleh yang wajib untuk anda bawa pulang ketika mengunjungi kota Tegal. Tak heran, jika negara Indonesia ini memang terkenal dengan berbagia jenis kerupuk yang tersebar di seluruh penjuru Indonesia. Untuk itu, di Tegal sendiri pun anda bisa mencoba untuk mecicipi kerupuk antor yang memiliki ukuran yang lebih kecil dari kerupuk upil. Untuk anda yang ingin mencobanya, maka anda pun bisa dengan mudah untuk membelinya di beberapa pasar tradisional yang terletak di kota Tegal. Dengan memiliki aroma dan cita rasa yang lebih kuat daripada kerupuk ini, maka tak heran jika bagi wisatawan yang menyukai kerupuk, maka juga akan membawa oleh – oleh khas satu ini ketika akan kembali ke kampung halaman."
+    },
+    {
+        title : "Kamir",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Kamir.jpg",
+        content : "Kue yang terbuat dari tape singkong dan tepung terigu yang dicampur dengan susu putih ini, juga menjadi salah satu alternatif pilihan yang bisa untuk anda bawa pulang sebagai buah tangan. Memiliki cita rasa yang enak, maka kue satu ini pun juga mampu bertaman selama 1 hari dalam suhu ruangan biasa dan dapat bertaham sekitar dua hari ketika disimpan di dalam kulkas. Untuk itu, anda pun bisa membawa pulang oleh – oleh satu ini yang bisa untuk anda dapatkan di pinggir jalan. Bagi anda yang sulit untuk menemukannya, maka anda bisa menyusuri sepanjang jalan RSUD Kardinah hingga jalan Rumah Bersalin di daerah Tegal untuk bisa menemukan jajanan khas dari kota Tegal ini."
+    },
+    {
+        title : "Manisan Cerme",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Manisan-Cerme.jpg",
+        content : "Makanan satu ini memang terkenal sebagai salah satu makanan khas kota Tegal. Anda pun dapat menemukan manisan ini dengan mudah dan kerap kali dijadikan buah tangan ketika wisatawan berkunjung ke kota ini. Manisan yang berasal dari buah cerme ini pun memiliki rasa yang sangat khas dan akan membuat anda ketagihan. Untuk itu, anda bisa membelinya di daerah Guci, dikarenakan mayoritas masyarakat memang berprofesi sebagai petani cerme."
+    },
+    {
+        title : "Pilus Kletuk",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Pilus.jpg",
+        content : "Ketika berkunjung ke Kota Tegal, pastikan anda untuk membeli camilan satu ini sebagai buah tangan. Pilus di kota ini pun memiliki rasa yang cenderung gurih sehingga akan sangat cocok untuk anda nikmati sebagai pendamping makanan berkuah. Pilus yang berasal dari Tegal ini tidak mengandung bahan pengawet dan mampu bertahan hingga 1 bulan dengan keadaan tertutup rapat sehingga akan tetap dalam keadaan yang renyah."
+    },
+    {
+        title : "Kacang Bogares",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Kacang-Bogares.jpg",
+        content : "Satu lagi oleh –oleh khas kota Tegal yang bisa untuk anda bawa pulang adalah kacang Bogares. Kacang satu ini memiliki rasa yang cukup unik sehingga beda dengan kacang kulit lainnya. Untuk itu, ketika anda sedang berada di Tegal cemilan satu ini wajib untuk anda bawa pulang. Anda bisa menemuakan kacang kulit ini di tempat seperti pasar tradisional ataupun pusat oleh-oleh di kota Tegal."
+    },
+    {
+        title : "Keripik Tahu",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Keripik-Tahu.jpg",
+        content : "Keripik satu ini pun juga bisa untuk anda jadikan buah tangan saat sedang berada di kota Tegal. Pada umumnya, keripik satu ini terbuat dari jenis tahu kuning yang lebih padat jika dibandingkan dengan tahu putih biasa. Untuk itu, hal inilah yang mendasari pembuatan keripik tahu tersebut. Keripik satu ini pun mempunyai rasa yang renyah dan gurih serta juga tidak  meninggalkan tekstur asli tahu yang cenderung lembut."
+    },
+    {
+        title : "Gemblong Ocar – Acir",
+        image : "https://cdn.idntimes.com/content-images/post/20180813/7c2204441ebf3f72bfb63988255fe9d9.jpg",
+        content : "Makanan ini mungkin terdengar asing dan mempunyai cita rasa yang cukup menggiurkan sehingga anda pun bisa menikmatinya. Anda bisa menemukan makanan satu ini bisa dengan mudah untuk anda temukan di pasar tradisional dan dibanderol dengan harga sekitar 5 ribu rupiah saja."
+    },
+    {
+        title : "Olos",
+        image : "https://cdn.idntimes.com/content-images/post/20180813/f8c87a3fe2c8e20bdfdcd62ab02ca3dd.jpg",
+        content : "Olos adalah makanan favorit di kalangan muda-mudi kota Tegal, lho. Camilan berbentuk bulat ini berisi sayuran dan cabai. Olos banyak dijual dalam gerobak-gerobak yang biasanya ada di depan sekolah-sekolah. Pecinta pedas jangan sampai melewati jajanan yang satu ini, ya. Soal harga jangan khawatir, satu biji olos hanya Rp500."
+    },
+    {
+        title : "Kue tempel",
+        image :"https://cdn.idntimes.com/content-images/community/2018/08/c871959614869fa7f54c0e99c7d736a7.jpg",
+        content : "Kue tempel terbuat dari tepung beras yang dipipihkan di wajan lalu diberi pisang, gula jawa dan kelapa muda sebagai isinya. Kamu bisa membeli kue ini di jalan HOS Cokroaminoto kota Tegal. Harganya sekitar Rp5.000 saja per porsi."
+    },
+    {
+        title : "Glotak",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/b61c30d9d57f4157e8a1c00f13bd2536.jpg",
+        content : "Makanan dengan bahan utama gembus ini disebut glotak dikarenakan pada saat proses memasaknya menimbulkan suara glotak-glotak dari tulang ayam yang bergesekan dengan panci. Glotak semakin sedap oleh tambahan beberapa rawit di dalamnya. Seporsi glotak biasanya hanya dibanderol Rp5.000 lho."
+    },
+    {
+        title : "Rujak teplak",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/ec870cae0fd2cd364edd68b7481ca8c9.jpg",
+        content : "Apa sih yang membedakan rujak teplak khas Tegal dengan rujak-rujak lainnya? Rujak teplak ini memiliki sambal yang khas karena ada campuran singkongnya. Rujak teplak terdiri dari aneka macam sayuran mulai dari kangkung, daun singkong, pare, kol dan tauge. Makan rujak teplak semakin nikmat dengan kerupuk. Apalagi seporsi rujak ini hanya Rp5.000 dan banyak dijual di pasar tradisional kota Tegal."
+    },
+    {
+        title : "Es sagwan",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/1710fec2814e2fc6f7ed7ab4910fc08e.jpg",
+        content : "Kenyang menikmati jajanan khas Tegal, saatnya minum yang segar-segar, ya. Cobalah mencicipi kesegaran dari es yang di dalamnya terdapat butiran panjang berwarna putih yang terbuat dari tepung kanji. Mau pilih rasa strawberry atau cokelat sesuaikan saja dengan seleramu. Segelas es sagwan ini dibanderol hanya dengan harga Rp3.000 saja."
+    },
+    {
+        title : "Teh poci",
+        image : "https://cdn.idntimes.com/content-images/community/2018/08/4e90be39fa3c67ae13029e5d75be04e8.jpg",
+        content : "Teh poci atau moci sudah menjadi ciri khas masyarakat Tegal. Aroma teh dengan rasa sedikit sepet ini sangat khas. Menikmati teh poci paling tepat saat masih hangat sedikit panas. Harganya berkisar Rp7.000,00 untuk satu packnya."
     }
 ]
