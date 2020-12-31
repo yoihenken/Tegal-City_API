213ebd6d372316126373918a95213c090a36b90c
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2020-12-31 17:00:27 +0700

    Add API data-kuliner.js

---
+++ app/data/data-kuliner.js
@@ -0,0 +1,124 @@
+var listKuliner = [
+    {
+        title : "Warung Pi'an",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Warung-Pi%E2%80%99an-aneka-masakan-nikmat.jpg",
+        address : "Jl. Kolonel Sudiarto, Slerok, Kec. Tegal Tim., Kota Tegal, Jawa Tengah 52122",
+        content : "Menu masakan yang ditawarkan ada aneka olahan dan Ada juga aneka cemilan untuk sekedar mengganjal perut seperti mendoan, pisang goreng dan lainnya. Menu andalan Warung Pi’an adalah Nasi Lengko. Nasi lengko adalah sepiring nasi yang diberi tambahan potongan tempe, tahu, taoge dan timun lalu disiram dengan saus kacang pedas."
+    },
+    {
+        title : "Sauto Tegal di Warung Makan Moro Tresno",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Sauto-Tegal-di-Warung-Makan-Moro-Tresno.jpg",
+        address : "Pasar Senggol Tegal, Jl. KH. Mansyur, Mangkukusuman, Kec. Tegal Tim., Kota Tegal, Jawa Tengah 52131",
+        content : "Berkunjung atau singgah di Tegal tidak lengkap rasanya bila tidak mencicipi citarasa dari Sauto Tegal yang khas. Sauto adalah nama yang disematkan oleh penduduk lokal untuk menyebut kata soto. Tampilan dari Soto Tegal lebih keruh dari biasanya karena ada campuran touge. Ada tambahan touce menjadikan cita rasa yang lebih kaya dengan perpaduan asin, manis dan gurih. Warung Makan Moro Tresno adalah salah satu tempat makan Soto Tegal yang terkenal enak."
+    },
+    {
+        title : "Kupat Glabed Pak Machali",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Kupat-Glabed-Pak-Machali.jpg",
+        address : "Randugunting, Kec. Tegal Sel., Kota Tegal, Jawa Tengah",
+        content : "Kupat Glabed punya tampilan yang mirip dengan lontong opor dengan kuah kuningnya. Kata Glabed memiliki arti kental sesuai dengan jenis kuah yang dipakai yaitu kuah santan yang memiliki rasa gurih. Ciri khas yang paling mencolok dari Kupat Glabed adalah kerupuk kuningnya. Suwiran ayam dipadukan dengan nikmatnya sate blengong jadi pilihan tempat makan malam dari Warung Pak Machali. Kupat Glabed Pak Machali salah satu tempat makan kupat legendaris yang sudah ada sejak lama."
+    },
+    {
+        title : "Rujak Tamplak",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Rujak-Tamplak-makanan-khas-sederhana-dari-Tegal.jpg",
+        address : "Jl. Letjen. Suprapto No.77-89, Kraton, Kec. Tegal Barat, Kota Tegal.",
+        content : "Rujak tamplak terdiri dari aneka sayuran yang sudah direbus seperti daun pepaya, daun lembayung, kangkung, daun ubi, semanggi dan taoge. Ciri khas dari rujak tamplak ini terletak pada sambal yang dipakai yang disebut dengan nama sambal gaul. Sambal gaul terbuat dari singkong yang dipadukan dengan aneka bumbu bercita rasa pedas. Rasakan nikmatnya kuliner sederhana dari Tegal yang punya perpaduan rasa gurik dan segar layaknya rujak."
+    },
+    {
+        title : "Sate Mendo Wendy’s",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Sate-Mendo-Wendy%E2%80%99s.jpg",
+        address : "Jl. Letjen. Suprapto Griya Mejasem Baru 4 No.59, Kraton, Kec. Tegal Bar., Kota Tegal, Jawa Tengah 52112",
+        content : "Tegal memiliki ciri khas pada olahan sate yaitu tekstur yang lembut serta tidak mengeluarkan aroma pada dagingnya. Jenis sate yang paling terkenal adalah sate kambing. Salah satu tempat makan sate kambing enak ada dari Sate Mendo Wendy’s. Rahasia kelezatan dari Sate Tegal terletak pada penggunaan daging yang dipakai, yaitu pemilihan daging kambing muda yang berusia dibawah tiga bulan, masih sangat kecil dan disebut sebagai batibul. Sate Tegal jadi kuliner wajib dicoba."
+    },
+    {
+        title : "Ikan Bakar Mekar Tegal",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Ikan-Bakar-Mekar-Tegal.jpg",
+        address : "Jl. Kapten Piere Tendean No.43, RT.05/RW.07, Tegalsari, Kec. Tegal Bar., Kota Tegal, Jawa Tengah 52111",
+        content : "Mendapat julukan Kota Bahari di Tegal Anda akan menemukan beragam olahan dari hasil lautnya. Salah satu tempat makan yang menyediakan menu olahan ikan adalah Rumah Makan Mekar Tegal. Tidak hanya ikan pembeli juga bisa menikmati aneka olahan seafood. Menu andalan Rumah Makan Mekar Tegal ada ikan bakar, udang asam manis, cumi saus asam manis dan lainnya. Tertarik untuk menikmati kelezatan ikan bakar khas Tegal jangan lupa untuk mampir ke Rumah Makan Mekar."
+    },
+    {
+        title : "Bakso Rudal Tegal",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Bakso-Rudal-Tegal.jpg",
+        address : "Jl. KS. Tubun Kelurahan No.115, Debong Tengah, Kec. Tegal Selatan, Kota Tegal.",
+        content : "Bakso makanan yang hampir ada di setiap daerah di Indonesia. Bakso dari Tegal yang sedang hits sekarang ada Bakso Rudal. Kenapa bisa hits karena di Pondok Bakso Rudal Tegal ini menyediakan Bakso dalam ukuran yang besar sebesar bola tenis. Isian bakso ada daging cincang halus, sebagai pelengkapnya diberikan tambahan mie, bihun, toge, irisan daun bawang dan bawang goreng. Nama bakso rudal begitu terkenal di Tegal. Bakso Rudal cocok dinikmati di segala kondisi."
+    },
+    {
+        title : "Tahu Murni Putra",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Tahu-Murni-Putra.jpg",
+        address : "Jl. AR. Hakim No.100, Mangkukusuman, Kec. Tegal Tim., Kota Tegal, Jawa Tengah 52131",
+        content : "Tahu murni salah satu jajanan khas dari Tegal. Tahu murni ini menyediakan berbagai olahan tahu seperti tahu aci, tahu pletok, kerupuk tahu, tahu royal, pepes tahu dan olahan tahu lainnya dengan citarasa gurih, lembut dan pedas. Tahu Murni dari rumah makan Nata Jaya adalah salah satu tempat Anda bisa mendapatkan olahan tahu Murni yang enak. Tahu murni cocok dipakai sebagai oleh-oleh khas dari Tegal."
+    },
+    {
+        title : "Iga Bakar Valentina",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Iga-Bakar-Valentina.jpg",
+        address : "Jl. Waringin No.35,kel, Mintaragen, Kec. Tegal Timur, Kota Tegal.",
+        content : "Wisata kuliner di Tegal yang paling terkenal dan populer ada iga bakar valentina. Iga bakar merupakan olahan iga sapi yang direbus lama sampai empuk baru kemudian dibakar. Rasa dari iga bakar valentina sudah tidak perlu diragukan lagi dengan tekstur iga yang super empuk. Untuk masalah harga tenang masakan iga bakar valentina ini masih ramah dikantong. Selain iga bakar di lesehan valentina ada menu lain seperti nasi soto, rawon, ayamn kampung dan aneka menu minuman."
+    },
+    {
+        title : "Belendung",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Belendung.jpg",
+        address : "Blendung, Bojongsana, Kec. Suradadi, Kota Tegal.",
+        content : "Wisata kuliner di Tegal selanjutnya adalah Belendung. Makanan khas ini umumnya dijual di pasar tradisional. Belendung adalah olahan dari jagung yang sudah direbus lalau diberikan bumbu berupa parutan kelapa dan sedikit garam. Rasa yang gurih dengan sedikit asin menjadikan Belendung jajanan khas Tegal yang wajib dicoba. Belendung bisa jadi menu sarapan pagi penanti nasi untuk Anda yang sedang menjalankan program diet."
+    },
+    {
+        title : "Es Lontrong",
+        image : "https://www.itrip.id/wp-content/uploads/2020/01/Es-Lontrong.jpg",
+        address : "Jl. Letjen Suprapto No.26, Slawi, Slawi Wetan, Kec. Slawi, Kota Tegal.",
+        content : "Ada Es lontrong yang super murah Cuma 4 ribu saja Anda sudah bisa menikmati segarnya es legendaris di Tegal ini. Es Lontrong perpaduan dari cincau, kacang hijau, gula merah dan santan. Es Lontrong ini masuk dalam jajaran streat food di Tegal."
+    },
+    {
+        title : "Warung Makan Miraos",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Warung-Makan-Miraos.jpg",
+        address : "Pekauman, West Tegal, Jl. Jend. Sudirman No.5, Randugunting, Kec. Tegal Sel., Kota Tegal, Jawa Tengah 52125",
+        content : "Warung makan ini seperti warteg yang menjual makanan dengan aneka lauk yang beragam. Di warung makan Miraos Anda bisa menemukan berbagai makanan khas Tegal seperti sego panggol, sauto hingga nasi lengko. Menariknya warung makan ini buka selama 24 jam sehingga Anda bisa mampir kapanpun. Warung makan ini juga miliki tempe mendoan dengan rasa gurih yang menjadi primadonanya, jangan lupa untuk mencobanya."
+    },
+    {
+        title : "Warung Makan Sauto Pak Da'an",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Warung-Makan-Sauto-Pak-Da%E2%80%99an.jpg",
+        address : "Jl. Raya Talang No.320, RW.2, Wirantakan, Talang, Kec. Talang, Tegal, Jawa Tengah 52193",
+        content : "Rumah makan di Tegal ini selalu ramai pengunjung dengan berbagai plat nomor kendaraan. Sauto merupakan kepanjangan dari soto tauco yang memiliki cita rasa unik dan mengunggah. Ada rasa manisnya, asim dan juga sedikit asam dengan kuah gelap yang menggoda selera. Di dalam Sauto sendiri terdapat isian suwiran daging ayam, irisan kol, tauco dan juga daun bawang. Umumnya nasi sudah masuk di dalam mangkuk, namun Anda juga bisa meminta dipisah."
+    },
+    {
+        title : "Rumah Makan Cempako Tegal",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Rumah-Makan-Cempako-Tegal.jpg",
+        address : "JL. Raya Pantura Pemalang, KM 20, Demangharjo, Warurejo, Tegal, Kademangan, Demangharjo, Warureja, Tegal, Central Java 52183",
+        content : "Tempat makan satu ini menawarkan menu ikan patin pedas dengan rasa yang sangat lezat. Menu lainnya yang juga wajib coba adalah pindang patin, gulai tempoyak, manggut patin hingga pepes patin. Rumah makan yang terletak di Jl. Pantura Pemalang Km 20 ini sangat cocok untuk Anda penggemar patin. Jika Anda tidak menyukainya bisa memesan menu sop buntut, empal hingga daging ayam atau yang lainnya."
+    },
+    {
+        title : "Restoran Pringsewu",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Restoran-Pringsewu.jpg",
+        address : "Area Sawah/Kebun, Harjasari, Kec. Suradadi, Tegal, Jawa Tengah 52182",
+        content : "Lokasinya yang strategis dengan berbagai macam menu tentunya membuat Anda dan keluarga bisa makan dengan nyaman dan enak. Rumah makan ini juga buka selama 24 jam sehingga Anda bisa mampir kapan saja ketika perut Anda keroncongan. Fasilitasnya juga terbilang lengkap dan juga ramah anak anak."
+    },
+    {
+        title : "Lontong Cap Go Meh Warung Ibu Wing",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Lontong-Cap-Go-Meh-Warung-Ibu-Wing.jpg",
+        address : "Jl. Waringin No.35,kel, Mintaragen, Kec. Tegal Timur, Kota Tegal.",
+        content : "Meskipun memiliki nama yang identik dengan perayaan Imlek, namun Anda bisa menikmati kuliner ini setiap hari. Warung yang menjual menu ini dengan rasa yang telah terpercaya adalah Warung Ibu Wing. Lontong Cap Go Meh ini secara tampilan memang mirip dengan lontong sayur dengan rasa yang berbeda. Semakin istimewa dengan tambahan telur dan suwiran ayam. Anda bisa langsung saja menuju Jl. Jenderan Sudirman, Siawi, Tegal."
+    },
+    {
+        title : "Sop Kikil Bu Umi",
+        image : "https://www.javatravel.net/wp-content/uploads/2020/01/Sop-Kikil-Bu-Umi.jpg",
+        address : "Jl. Tegal – Cilacap, No.25, Pekauman Kulon, Dukuhturi, Kec. Dukuhturi, Kab. Tegal.",
+        content : "Menikmati sop kikil ini akan membuat perjalanan kuliner Anda semakin lengkap. Sup kikil warung bu Umi ini memiliki kuah yang gurih dengan kikil yang kenyal yang bisa membuat Anda ketagihan. Lebih nikmat lagi jika disantap dengan nasi hangat yang pulen. Sop kikil ini terletak di dekat SMK 1 Dukuhturi dan akan buka pada siang hari saja. Bumbunya yang khas dijamin tak akan membuat Anda menyesal mencobanya."
+    },
+    {
+        title : "Warung Sate Ayam Margasari Tegal",
+        image : "https://www.cintapekalongan.com/wp-content/uploads/2020/05/Warung-Sate-Ayam-Margasari-Tegal.jpg",
+        address : "Jl. Jenderal Ahmad Yani, No. 188, Mintaragen, Mangkukusuman, Kec. Tegal Tim., Kota Tegal, Jawa Tengah 52125",
+        content : "Salah satu restoran atau warung sate ayam yang terkenal dan enak di Tegal yaitu Sate Ayam Margasari. Selain Sate ayam juga ada sate kulit, sate ati ampela dan campuran sate semuanya. Warung sate ini menggunakan kuah kuning yang dipadukan dengan lontongnya yang membuat rasanya enak, dagingnya empuk, bumbu kacangnya legit. Sate Ayam Margasari tidak terlalu besar tapi rasanya enak dengan lokasinya yang mudah terlihat dari kepulan asap satenya."
+    },
+    {
+        title : "Ayam Goreng Spesial Lombok Idjo Tegal",
+        image : "https://www.wisatadanhotelmurah.com/wp-content/uploads/2018/05/Ayam-Goreng-Spesial-Lombok-Idjo-Tegal.jpg",
+        address : "Jl. Raya Karang Anyar No.21A, Pekauman Kulon, Dukuhturi, Tegal, Jawa Tengah",
+        content : "Merupakan tempat makan enak di Tegal dengan menu-menu utama ayam goreng yang enak dan lezat. Disamping ayam juga menyediakan menu-menu lainnya dengan rasa yang lezat juga seperti makanan dari ikan, dari daging sapi dan lainnya disamping juga menu-menu minuman."
+    },
+    {
+        title : "Tahu Aci Sari Eco",
+        image : "https://cdn.idntimes.com/content-images/community/2017/12/resep-tahu-aci-51c6c69ed357aab0517fbc4717f1365d.jpg",
+        address : "Jl. AR. Hakim No.48, Randugunting, Kec. Tegal Sel., Kota Tegal, Jawa Tengah 52131",
+        content : "Sering disebut juga dengan Tahu Tegal atau Tahu Kuping, Tahu Aci terbuat dari tahu kuning yang berbentuk persegi empat yang kemudian dibelah dua secara melintang, diberi adonan yang terbuat dari tepung kanji atau aci, potongan daun kucai, dan beberapa bumbu lainnya. Belum lengkap rasanya berkunjung ke Tegal jika belum mencicipi Tahu Aci. Konon, tahu kuning ini ternyata banyak diproduksi di daerah kabupaten Tegal saja. Oleh karena itu, Tahu Aci hanya bisa kamu temukan di kota Tegal saja."
+    }
+]
+
+module.exports = { listKuliner };
